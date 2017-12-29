384f5f292bbfab78c0f9d4b237d3b770f62c05ad
jhonatacaiob <34169712+jhonatacaiob@users.noreply.github.com>
2017-12-29 14:27:50 -0300

    Update SignInUser.py

--- SignInUser.py
+++ SignInUser.py
@@ -1,37 +1,49 @@
 def cadastramento2():
     from CARTELAS3 import cadastramento1
     NomeJogador = input("Qual seu nome?: ")
-    preco = ''
+    preco = 0
+    total = []
     comece = 's'
     todas_apostas = []
     while comece == "sim" or comece == "s":
         printavel, Apostas = cadastramento1()
         if Apostas == 6:
-            preco += '3,50'
+            preco += 3.5
+            total += [preco]
         elif Apostas == 7:
-            preco += '4,50'               
+            preco += 24.5
+            total += [preco]
         elif Apostas == 8:
-            preco += '98'
+            preco += 98
+            total += [preco]
         elif Apostas == 9:
-            preco += '294'
+            preco += 294
+            total += [preco]
         elif Apostas == 10:
-            preco += '735'
+            preco += 735
+            total += [preco]
         elif Apostas == 11:
-            preco += '1.617'
+            preco += 1617
+            total += [preco]
         elif Apostas == 12:
-            preco += '3.234'
+            preco += 3234
+            total += [preco]
         elif Apostas == 13:
-            preco += '6006'
+            preco += 6006
+            total += preco
         elif Apostas == 14:
-            preco += '10.510,5'
+            preco += 10510.5
+            total += [preco]
         elif Apostas == 15:
-            preco += '17.517,50'
-        print("Sr(a). %s, você apostou nos numeros %s" %(NomeJogador, printavel))
-        print("Pague R$: %s antes de sair" %(preco))
-        todas_apostas += [printavel] 
+            preco += 17517.5
+            total += [preco]
+        print("Sr(a). %s, vocÃª apostou nos numeros %s" %(NomeJogador, printavel))
+        print("Essa aposta deu R$:",preco)
+        todas_apostas += [printavel]
+        preco = 0
         comece = input("gostaria de jogar novamente?: ")
-        preco = ''
     else:
-        print("OBRIGADO POR USAR NOSSOS SERVIÇOS")
+        print("Pague R$:",sum(total),"antes de sair")
+        print("OBRIGADO POR USAR NOSSOS SERVIÃ‡OS")
     apostas = '|'.join(todas_apostas)
     return apostas, NomeJogador
